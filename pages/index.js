6884acc0f2da7554a07631123a34e3f2fcb2fce4
Vikrant Gajria <vikrantgajria@gmail.com>
2019-03-10 14:00:40 +0530

    Remove spaces

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default () => (
 						<ReportsTable />
 					</Col>
 				</Row>
-	        	<br/><br/>
+
 	        	<Row>
 	        		<Col xs="12" sm="12" md="6" lg="6" className="my-3">
 	        			<img className="img-fluid" src="../static/images/principal.jpg" />
